f2e7067e1c9becdff209c74a7660473a46e734a6
john paul <johnjeneiro@gmail.com>
2021-07-09 15:33:08 +0100

    nnn

--- index.js
+++ index.js
@@ -19,22 +19,9 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, Accept,Authorization,Origin"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  req.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-app.options("*", cors());
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+//app.use(cors({ origin: ["https://apexwavefintech.netlify.app"], credentials: true }));
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 app.use;
 
