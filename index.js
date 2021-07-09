cbfe84d82df1370e0228904c874c6e3bc989a890
john paul <johnjeneiro@gmail.com>
2021-07-09 15:26:16 +0100

    nn

--- index.js
+++ index.js
@@ -19,16 +19,23 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.use(cors());
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+app.use(function (req, res, next) {
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
   );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  req.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
+
+app.options("*", cors());
+
 app.use;
 
 app.use("/records", require("./routers/recordsRouter"));
