9639fe3b772078dc32daa1c1610355f46adf0fd0
john paul <johnjeneiro@gmail.com>
2021-07-09 15:08:02 +0100

    mm

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors());
-app.options("*", cors());
+app.all("*", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+});
 app.use;
 
 app.use("/records", require("./routers/recordsRouter"));
