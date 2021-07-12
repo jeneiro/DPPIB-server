174e45863808efc4e6b43b05034d13ce980c0971
john paul <johnjeneiro@gmail.com>
2021-07-12 09:25:10 +0100

    mmm

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const app = express();
 const cors = require("cors");
 app.use(cors());
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`server started on : ${PORT}`);
 });
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use("/records", require("./routers/recordsRouter"));
 app.use("/release", require("./routers/releaseRouter"));
